b13ffd8bf1e235d885fe469b12e7df2e6506daf8
flavius1102 <veronika.laczo@gmail.com>
2020-08-06 09:40:01 +0200

    Added config; added login/reg pages; added footer

--- ajax/register.php
+++ ajax/register.php
@@ -11,12 +11,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 
     $email = Filter::String($_POST['email']);
 
-    // Make sure the user does not exist
-    $findUser = $con->prepare("SELECT user_id FROM users WHERE email = LOWER(:email) LIMIT 1");
-    $findUser->bindParam(':email', $email, PDO::PARAM_STR);
-    $findUser->execute();
+    $user_found = findUser($con, $email);
 
-    if ($findUser->rowCount() == 1) {
+    if ($user_found) {
         // User exists
         // We can also check to see is they are able to log in
         $return['error'] = "You already have an account";
--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,21 @@ require_once 'inc/config.php';
 
 forceLogin();
 
+$user_id = $_SESSION['user_id'];
+
+$getUserInfo = $con->prepare("SELECT email, reg_time FROM users WHERE user_id= :user_id LIMIT 1");
+$getUserInfo->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+$getUserInfo->execute();
+
+if ($getUserInfo->rowCount() == 1) {
+    // User was found
+    $User = $getUserInfo->fetch(PDO::FETCH_ASSOC);
+} else {
+    // User is not signed in
+    header("Location: logout.php");
+    exit;
+}
+
 ?>
 
 <!<!doctype html>
@@ -16,7 +31,7 @@ forceLogin();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Dashboard</title>
     <base href="/">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css">
 </head>
@@ -24,7 +39,8 @@ forceLogin();
 
 <div class="uk-section uk-container">
     <h2>Dashboard</h2>
-    <p>You are signed in as user: <?php echo $_SESSION['user_id']; ?></p>
+    <p>Hello <?php echo $User['email']; ?>, you registered at <?php echo $User['reg_time']; ?></p>
+    <p><a href="php_login_course/logout.php">Logout</a></p>
 </div>
 
 <?php require_once "inc/footer.php"; ?>
